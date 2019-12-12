746720f243f87bbdaf51bc022f2454893252e2ad
pine <shenchaosong@yidaoit.com>
2019-12-12 20:35:01 +0800

    init model

--- src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
+++ src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
@@ -1,45 +1,24 @@
 package geektime.spring.data.datasourcedemo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 @Slf4j
-public class DataSourceDemoApplication implements CommandLineRunner {
-	@Autowired
-	private DataSource dataSource;
-
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+public class DataSourceDemoApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataSourceDemoApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		showConnection();
-		showData();
+	public void run(ApplicationArguments args) throws Exception {
 	}
 
-	private void showConnection() throws SQLException {
-		log.info(dataSource.toString());
-		Connection conn = dataSource.getConnection();
-		log.info(conn.toString());
-		conn.close();
-	}
-
-	private void showData() {
-		jdbcTemplate.queryForList("SELECT * FROM FOO")
-				.forEach(row -> log.info(row.toString()));
-	}
 }
 
