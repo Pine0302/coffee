9aacab64ec01141f75707a7b166243e37e28439f
shenchaosong <shenchaosong@yidaoit.cn>
2020-08-24 16:09:13 +0800

    缓存demo

--- src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
+++ src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
@@ -17,6 +17,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -35,68 +36,82 @@ import java.util.Optional;
 @EnableJpaRepositories
 @EnableTransactionManagement
 @Slf4j
+@EnableCaching(proxyTargetClass = true)
 public class DataSourceDemoApplication implements ApplicationRunner {
-	@Autowired
-	private CoffeeRepository coffeeRepository;
-	@Autowired
-	private CoffeeOrderRepository orderRepository;
-	@Autowired
-	private CoffeeService coffeeService;
-	@Autowired
-	private CoffeeOrderService coffeeOrderService;
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+    @Autowired
+    private CoffeeOrderRepository orderRepository;
+    @Autowired
+    private CoffeeService coffeeService;
+    @Autowired
+    private CoffeeOrderService coffeeOrderService;
 
-	@Autowired
-	private MongoTemplate mongoTemplate;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-	@Autowired
-	public MongoCustomConversions mongoCustomConversions(){
-		return new MongoCustomConversions(Arrays.asList(new MoneyReadConverter()));
-	}
+    @Autowired
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(Arrays.asList(new MoneyReadConverter()));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataSourceDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataSourceDemoApplication.class, args);
+    }
 
-	@Override
-	@Transactional
-	public void run(ApplicationArguments args) throws Exception {
-		runMain();
-		//runMongo();
-	}
+    @Override
+    @Transactional
+    public void run(ApplicationArguments args) throws Exception {
+        //runMain();
+        //runMongo();
+        runCache();
+    }
 
-	public void runMain(){
-		//get all coffee
-		log.info("ALl Coffee: {}", coffeeRepository.findAll());
+    public void runMain() {
+        //get all coffee
+        log.info("ALl Coffee: {}", coffeeRepository.findAll());
 
-		//order a coffee and change order status
-		Optional<Coffee> latte = coffeeService.findOneCoffee("latte");
-		log.info("get a Coffee: {}", latte);
+        //order a coffee and change order status
+        Optional<Coffee> latte = coffeeService.findOneCoffee("latte");
+        log.info("get a Coffee: {}", latte);
 
-		if(latte.isPresent()){
-			CoffeeOrder order = coffeeOrderService.saveCoffeeOrder("pine",latte.get());
-			log.info("saved order: {}",order);
-			log.info("from init to paid: {}",coffeeOrderService.updateState(order, OrderState.PAID));
-			log.info("from paid to init: {}",coffeeOrderService.updateState(order, OrderState.INIT));
-		}
+        if (latte.isPresent()) {
+            CoffeeOrder order = coffeeOrderService.saveCoffeeOrder("pine", latte.get());
+            log.info("saved order: {}", order);
+            log.info("from init to paid: {}", coffeeOrderService.updateState(order, OrderState.PAID));
+            log.info("from paid to init: {}", coffeeOrderService.updateState(order, OrderState.INIT));
+        }
 
-	}
+    }
 
-	public void runMongo()  {
-		MongoCoffee latte = MongoCoffee.builder()
-				.name("latte")
-				.price(Money.of(CurrencyUnit.of("CNY"),20.0))
-				.createTime(new Date())
-				.updateTime(new Date())
-				.build();
-		MongoCoffee saved = mongoTemplate.save(latte);
-		log.info("saved coffee: {}",saved);
+    public void runMongo() {
+        MongoCoffee latte = MongoCoffee.builder()
+                .name("latte")
+                .price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+        MongoCoffee saved = mongoTemplate.save(latte);
+        log.info("saved coffee: {}", saved);
 
-		List<MongoCoffee> list = mongoTemplate.find(
-				Query.query(Criteria.where("name").is("latte")),MongoCoffee.class
-		);
-		log.info("find { } Coffee",list.size());
-		list.forEach(c->log.info("Coffee {} ",c));
-	}
+        List<MongoCoffee> list = mongoTemplate.find(
+                Query.query(Criteria.where("name").is("latte")), MongoCoffee.class
+        );
+        log.info("find { } Coffee", list.size());
+        list.forEach(c -> log.info("Coffee {} ", c));
+    }
+
+    public void runCache() {
+        //get all coffee
+        log.info("ALl Coffee: {}", coffeeService.findAllCoffee().size());
+        for (int i = 0; i < 10; i++) {
+            log.info(" find from cache .");
+            coffeeService.findAllCoffee();
+        }
+        coffeeService.reloadCoffee();
+		log.info(" find after reload .");
+		coffeeService.findAllCoffee().forEach(c->log.info("coffee:{}",c.getName()));
+    }
 
 
 }
